bea42629e638788a3125f61f07e062eb2f65f574
Stepan K <stepankravtsov0402@gmail.com>
2023-04-18 23:33:07 +0300

    Initial Commit

---
+++ Classy/ClassyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassyApp.swift
+//  Classy
+//
+//  Created by Степан Кравцов on 18.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ClassyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
